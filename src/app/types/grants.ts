be15ed7cf781888ac81bb2c5045116221ba83aff
t-murch <todd.murchison@khoros.com>
2023-12-10 14:26:46 -0600

    Form improvements. Dropdown, calenndar, expand. Imagee component usage improvement.

--- src/app/types/grants.ts
+++ src/app/types/grants.ts
@@ -1,12 +1,11 @@
 export type Grant = {
   name: string;
-  orgName: string;
+  organizationName: string;
   deadline: DeadlineType;
   url: string;
   amount: number;
   description: string;
-  popServed: string;
-  indServed: string;
+  industryServed: string;
   dateAdded: string;
   lastUpdated: string;
   submitted: boolean;
