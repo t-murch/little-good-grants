6ded98a304eb88f3c512ad0bde6a5696d1df1466
Todd Murchison Jr <t.murchjr@gmail.com>
2024-03-04 15:49:31 -0600

    scrape job scaffolding is back

--- packages/core/src/types/grants.ts
+++ packages/core/src/types/grants.ts
@@ -1,13 +1,13 @@
-import { z } from "zod";
-import { v4 as uuid } from "uuid";
+import { z } from 'zod';
+import { v4 as uuid } from 'uuid';
 
-const UNDEFINED_DATE = "UNDEFINED_DATE";
-type DeadlineDate = "varying" | "ongoing";
+const UNDEFINED_DATE = 'UNDEFINED_DATE';
+type DeadlineDate = 'varying' | 'ongoing';
 
 const FormGrant = z.object({
-  amount: z.union([z.number(), z.literal("varies")]).optional(),
+  amount: z.union([z.number(), z.literal('varied')]).optional(),
   deadline_date: z.string().datetime(),
-  industries_served: z.union([z.literal("non-profit"), z.literal("profit")]),
+  industries_served: z.union([z.literal('non-profit'), z.literal('profit')]),
   name: z.string(),
   organization_name: z.string(),
   url: z.string().url(),
@@ -16,12 +16,12 @@ const FormGrant = z.object({
 type FormGrant = z.infer<typeof FormGrant>;
 
 const SparseGrant = z.object({
-  amount: z.union([z.number(), z.literal("varies")]),
+  amount: z.union([z.number(), z.literal('varied')]),
   deadline_date: z.union([z.string().datetime(), z.literal(UNDEFINED_DATE)]),
   deadline_type: z.union([
-    z.literal("varying"),
-    z.literal("ongoing"),
-    z.literal("fixed"),
+    z.literal('varying'),
+    z.literal('ongoing'),
+    z.literal('fixed'),
   ]),
   description: z.string().optional(),
   industries_served: z.string(),
@@ -34,29 +34,29 @@ type SparseGrant = z.infer<typeof SparseGrant>;
 
 const Grant = z.object({
   ...SparseGrant.shape,
-  approved: z.union([z.literal("yes"), z.literal("no")]),
+  approved: z.union([z.literal('yes'), z.literal('no')]),
   date_added: z.string().datetime(),
   id: z.string().uuid(),
   last_updated: z.string(),
-  source: z.union([z.literal("user"), z.literal("scrape")]),
-  submitted: z.literal("x").optional(),
+  source: z.union([z.literal('user'), z.literal('scrape')]),
+  submitted: z.literal('x').optional(),
   submission_date: z.string().optional(),
   year_uuid: z.string(),
 });
 
 type Grant = z.infer<typeof Grant>;
 
-const parseAmount = (amount: string | number | null) => {
-  if (amount === null) return "varies";
-  if (typeof amount === "string" && amount.startsWith("$")) {
-    return Math.trunc(Number(amount.slice(1).split(",").join("")));
-  } else if (typeof amount === "string") return "varies";
+function parseAmount(amount: string | number | null): number | 'varied' {
+  if (amount === null) return 'varied';
+  if (typeof amount === 'string' && amount.startsWith('$')) {
+    return Math.trunc(Number(amount.slice(1).split(',').join('')));
+  } else if (typeof amount === 'string') return 'varied';
   return amount;
-};
+}
 
 function generatePartitionKey(uuid: string): string {
   if (!z.string().uuid().safeParse(uuid).success) {
-    throw new Error("Invalid UUID");
+    throw new Error('Invalid UUID');
   }
 
   const today = new Date();
@@ -68,18 +68,18 @@ function generatePartitionKey(uuid: string): string {
 // Map 1x1 SparseGrant to Grant
 function hydrateGrant(
   thirstyGrant: FormGrant | SparseGrant,
-  source: "user" | "scrape" = "user",
+  source: 'user' | 'scrape' = 'user',
 ): Grant {
   const id = uuid();
   const currentTime = new Date().toISOString();
 
   return Object.assign(thirstyGrant, {
     amount: parseAmount(thirstyGrant?.amount || null),
-    approved: "no",
+    approved: 'no',
     date_added: currentTime,
     deadline_type: (thirstyGrant.deadline_date === UNDEFINED_DATE
-      ? "ongoing"
-      : "fixed") as DeadlineDate,
+      ? 'ongoing'
+      : 'fixed') as DeadlineDate,
     id: id,
     last_updated: currentTime,
     source: source,
--- packages/frontend/src/app/api/scrape/scraper.ts
+++ packages/frontend/src/app/api/scrape/scraper.ts
@@ -1,30 +1,158 @@
-import { chromium } from 'playwright';
+import { Browser, chromium } from 'playwright';
+import { Grant, parseAmount } from '../../../../../core/src/types/grants';
+import { createWriteStream } from 'node:fs';
 
-async function scrapeNestedIframe() {
-  const browser = await chromium.launch();
-  const page = await browser.newPage();
+type ScrapeRow = {
+  clientRowID: string;
+  [key: string]: string;
+} & Grant;
+
+type LeftPaneRowMap = {
+  clientRowID: string;
+  name: string;
+};
+
+async function scrapeSite(): Promise<Grant[]> {
+  const normalizedData: Grant[] = [];
+  let browser: Browser | undefined = undefined;
 
   try {
-    // Navigate to the main page
-    await page.goto('https://www.example.com');
+    const startTime = Date.now();
+    browser = await chromium.launch({});
+    const page = await browser.newPage();
 
-    // Locate and switch to the iframe
-    const iframeHandle = await page.waitForSelector('iframe');
-    const iframeContent = await iframeHandle.contentFrame();
+    const LADIES_LAUNCH_URL =
+      'https://www.ladieswholaunch.org/small-business-grants';
+    const airTableClass = '.airtable-embed';
+    const returnPayload: { [key: string]: ScrapeRow } = {};
 
-    // Now you can interact with elements within the iframe
-    const nestedContent = await iframeContent.$eval(
-      'selector-inside-iframe',
-      (element) => element.textContent.trim(),
+    await page.goto(LADIES_LAUNCH_URL);
+    await page.waitForURL(LADIES_LAUNCH_URL);
+    console.log('WENT TO THE URL: %s', LADIES_LAUNCH_URL);
+
+    const tableContainer = page.frameLocator(airTableClass).first();
+    const countText = await tableContainer
+      .getByTestId('group-left-pane')
+      .first()
+      .locator('.count')
+      .innerText();
+    console.log('Table displays %s Open Grants', countText);
+
+    const leftPaneRows = tableContainer.getByTestId('data-row');
+    const leftPaneRowCount = await leftPaneRows.count();
+    const headerMap: { [key: string]: string } = {};
+
+    // Build Map<columnId, category>
+    const rightPanelHeaderContainer = tableContainer
+      .locator('.headerRow.rightPane')
+      .first();
+    const rightPanelHeaderKeys =
+      rightPanelHeaderContainer.locator('.cell.header');
+    const rightPaneHeaderRowCount = await rightPanelHeaderKeys.count();
+
+    for (let i = 0; i < rightPaneHeaderRowCount; i++) {
+      const thisKey = rightPanelHeaderKeys.nth(i);
+      const thisColumnId = await thisKey.getAttribute('data-columnid');
+      const columnName = await thisKey.locator('.name').innerText();
+      if (thisColumnId && columnName) headerMap[thisColumnId] = columnName;
+    }
+
+    const rightPanelData = tableContainer
+      .locator('.dataRightPaneInnerContent.paneInnerContent')
+      .locator('.dataRow.rightPane');
+    const rightPanelRowCount = await rightPanelData.count();
+
+    // Build initial list of rows with names
+    for (let i = 0; i < leftPaneRowCount; i++) {
+      const leftPaneRow: LeftPaneRowMap = {} as LeftPaneRowMap;
+      const thisRow = leftPaneRows.nth(i);
+
+      leftPaneRow.clientRowID =
+        (await thisRow.getAttribute('data-rowid')) || '';
+      const rawNameField = await thisRow.innerText();
+      const normalNameField = rawNameField.slice(1).trimStart();
+      leftPaneRow.name = normalNameField;
+
+      if (!returnPayload?.[leftPaneRow.clientRowID]) {
+        returnPayload[leftPaneRow.clientRowID] = leftPaneRow as ScrapeRow;
+      }
+    }
+
+    for (let i = 0; i < rightPanelRowCount; i++) {
+      const thisRow = rightPanelData.nth(i);
+      const correspondingRowID = await thisRow.getAttribute('data-rowid');
+
+      if (!correspondingRowID) break; // TO-DO: Add error logging/handling.
+
+      const rowData = thisRow.locator('.cell.read');
+      const rowPropertyCount = await rowData.count();
+
+      for (let j = 0; j < rowPropertyCount; j++) {
+        const thisProperty = rowData.nth(j);
+        const thisColumnId = await thisProperty.getAttribute('data-columnid');
+
+        if (!thisColumnId) break;
+        const cellText = await thisProperty.innerText();
+
+        if (returnPayload?.[correspondingRowID] && headerMap?.[thisColumnId]) {
+          returnPayload[correspondingRowID][headerMap[thisColumnId]] = cellText;
+        }
+      }
+    }
+
+    // Process and filter the data
+    normalizedData.push(
+      ...Object.entries(returnPayload)
+        .map(([_, grantRow]) => {
+          const rowCopy = { ...grantRow } as { [key: string]: string };
+
+          const cleanGrantDAO: Grant = {
+            amount: parseAmount(rowCopy?.['Amount']),
+            approved: 'no',
+            deadline_date: rowCopy?.['Deadline'],
+            description: rowCopy?.['Description'],
+            industries_served: rowCopy?.['Industries Served'],
+            name: rowCopy?.['name'],
+            organization_name: rowCopy?.['Organization Name'],
+            source: 'scrape',
+            url: rowCopy?.['URL'],
+          } as Grant;
+          return cleanGrantDAO;
+        })
+        .filter((grant) => {
+          const today = Date.now();
+          const monthDayYearDueDate = grant.deadline_date.split('/');
+
+          if (monthDayYearDueDate.length !== 3) return true;
+
+          const parsedDeadline = new Date(
+            parseInt(monthDayYearDueDate[2]),
+            parseInt(monthDayYearDueDate[0]) - 1,
+            parseInt(monthDayYearDueDate[1]),
+          );
+
+          return parsedDeadline.valueOf() > today;
+        }),
     );
 
-    console.log('Nested Content:', nestedContent);
-  } catch (error) {
+    // Log duration and results
+    const duration = ((Date.now() - startTime) / 1000).toFixed(3);
+    const streamDuration = createWriteStream('./timeResults.txt', {
+      flags: 'a',
+    });
+    const streamResults = createWriteStream('./scrapeResults.json');
+    streamDuration.write(duration + '--- \n');
+    streamResults.write(JSON.stringify(normalizedData, null, 2));
+    streamDuration.end();
+    streamResults.end();
+  } catch (error: any) {
     console.error('Error:', error.message);
   } finally {
-    await browser.close();
+    if (browser) await browser.close();
+    return normalizedData;
   }
 }
 
-// Call the scraping function
-scrapeNestedIframe();
+scrapeSite()
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error));
